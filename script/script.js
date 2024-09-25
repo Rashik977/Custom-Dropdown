e908166fab32ba45ee16e1908a433fdf7ee89be4
rashik <rkoirala43@gmail.com>
2024-09-25 13:59:21 +0545

    keyboard inputs & closing when clicked outside funtionality

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ const dropdownButton = document.querySelector(".dropdown-button");
 const dropdownText = document.querySelector(".dropdown-text");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 const dropdownArrow = document.querySelector(".dropdown-arrow");
+const dropdown = document.querySelector(".dropdown");
 
 let selectedItem = null;
 let previousSelectedItem = null;
@@ -9,9 +10,19 @@ let previousSelectedItem = null;
 const toggleDropdownMenu = () => {
   dropdownMenu.classList.toggle("show");
   dropdownArrow.classList.toggle("rotate");
+
+  if (dropdownMenu.classList.contains("show")) {
+    document.addEventListener("keydown", keyboardNavigation);
+  } else {
+    document.removeEventListener("keydown", keyboardNavigation);
+  }
 };
 
 const selectItem = (event) => {
+  // to prevent other items from being selected
+  if (!event.target.classList.contains("dropdown-item")) {
+    return;
+  }
   previousSelectedItem = selectedItem;
   selectedItem = event.target;
   dropdownText.textContent = selectedItem.textContent;
@@ -34,5 +45,21 @@ const selectedItemIconToggle = () => {
   previousSelectedItem.classList.remove("selected");
 };
 
+const keyboardNavigation = (event) => {
+  if (event.key === "Enter") {
+    selectItem(event);
+  }
+};
+
+const closeDropdownOutsideClick = (event) => {
+  // Check if the click is outside the dropdown
+  if (!dropdown.contains(event.target)) {
+    toggleDropdownMenu();
+    dropdownMenu.classList.remove("show");
+    dropdownArrow.classList.remove("rotate");
+  }
+};
+
+document.addEventListener("click", closeDropdownOutsideClick);
 dropdownMenu.addEventListener("click", selectItem);
 dropdownButton.addEventListener("click", toggleDropdownMenu);
